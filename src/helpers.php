eb36e6d4a9f6786a1bd31552c4a9ed35592de418
arturishe <artur@vis-design.com>
2016-01-19 17:29:34 +0200

    replace param

--- src/helpers.php
+++ src/helpers.php
@@ -2,16 +2,20 @@
 use Vis\Translations\Trans;
 
 //get translate
-function __($phrase) {
+function __($phrase, array $replacePhrase = []) {
     $this_lang = Lang::locale();
 
     $array_translate =  Trans::fillCacheTrans();
 
     if (isset($array_translate[$phrase][$this_lang])) {
-        return $array_translate[$phrase][$this_lang];
-    } else {
-        return $phrase;
+        $phrase = $array_translate[$phrase][$this_lang];
     }
+
+    if (count($replacePhrase)) {
+        $phrase = str_replace(array_keys($replacePhrase), array_values($replacePhrase), $phrase);
+    }
+
+    return $phrase;
 }
 
   function cmp($a, $b) {
